acefdf611b1072687f948bbf8644ab0b3342da4f
xiongyandong <1078464470@qq.com>
2021-03-03 14:46:59 +0800

    修改run文件，验证jenkins运行

--- run.py
+++ run.py
@@ -16,25 +16,27 @@ from tools.to_email import SendEmail
 from tools.project_path import ProjectPath, SaaSWebsitePath
 
 if __name__ == '__main__':
-    _type = input('接口测试输入1， ui测试输入2： ')
-    if _type == '1':
-        report_path = ProjectPath.p2p_report_path
-        report_name = 'p2p测试报告'+str(time.time())+'.html'
-        suites = TestSuit().make_suite_by_loader()
-        result = BeautifulReport(suites)
-        result.report(filename=report_name, description='登录充值单元测试报告', report_dir=report_path,
-                      theme='theme_cyan')
-        filepath = os.path.join(report_path, report_name)
-        if filepath:
-            SendEmail.send_email(filepath)
-    elif _type == '2':
-        report_path = SaaSWebsitePath.saaswebsite_report_path
-        report_name = 'ui测试报告'+datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')+'.html'
-        suites = SaaSwebsiteSuit().make_suite_by_loader(TestLogin)
-        result = BeautifulReport(suites)
-        result.report(filename=report_name, description='saas站登录测试报告', report_dir=report_path,
-                      theme='theme_cyan')
-        filepath = os.path.join(report_path, report_name)
-        if filepath:
-            SendEmail.send_email(filepath)
+    # _type = input('接口测试输入1， ui测试输入2： ')
+    # if _type == '1':
+    report_path = ProjectPath.p2p_report_path
+    report_name = 'p2p测试报告'+str(time.time())+'.html'
+    suites = TestSuit().make_suite_by_loader()
+    result = BeautifulReport(suites)
+    result.report(filename=report_name, description='登录充值单元测试报告', report_dir=report_path,
+                  theme='theme_cyan')
+    filepath = os.path.join(report_path, report_name)
+    if filepath:
+        SendEmail.send_email(filepath)
+    # elif _type == '2':
+    #     report_path = SaaSWebsitePath.saaswebsite_report_path
+    #     report_name = 'ui测试报告'+datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')+'.html'
+    #     suites = SaaSwebsiteSuit().make_suite_by_loader(TestLogin)
+    #     result = BeautifulReport(suites)
+    #     result.report(filename=report_name, description='saas站登录测试报告', report_dir=report_path,
+    #                   theme='theme_cyan')
+    #     filepath = os.path.join(report_path, report_name)
+    #     if filepath:
+    #         SendEmail.send_email(filepath)
+
+
 
